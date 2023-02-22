6951cc4cf895008a1280d8ea3540d342b3509b07
Laxman2012 <110541419+Laxman2012@users.noreply.github.com>
2023-02-22 21:20:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload(){
+
+}
+
+function setup()
+{
+
+   canvas= createCanvas(400,400);
+   canvas.position(110,250);
+   video = createCapture(VIDEO);
+   video.hide();
+
+   tint_color="";
+
+}
+
+function draw()
+{
+
+   image(video,0,0,400,400);
+   tint(tint_color);
+
+}
+
+function take_snapshot()
+{
+
+  save('Laxman_filter_photos');
+
+}
+
+function Filter_tint()
+{
+
+   tint_color = document.getElementById("color_name").value;
+
+}
